5a8cce66b672d77f64aa0d9605242eef04fbbe8b
lymarrie <lucmarrie1@gmail.com>
2023-07-21 15:28:22 -0400

    updated react-icons import

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import Slider from 'react-slick';
+import Slider, { Settings } from "react-slick";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { BiCaretRightCircle, BiCaretLeftCircle } from "react-icons/bi";
+import { BiCaretRightCircle, BiCaretLeftCircle } from "react-icons/bi/index";
 
 interface ArrowProps {
   className?: string;
